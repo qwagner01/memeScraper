265350d526ec46fa4e50672c05154190b6b0f31d
qwagner01 <qwagner01@gmail.com>
2018-10-18 13:45:42 -0400

    added praw

--- scraper.py
+++ scraper.py
@@ -1,10 +1,14 @@
-from bs4 import BeautifulSoup
-import requests
-# wvNg1MtuMnscZb_7XM_OkiEPmro
 import praw
+import config
+import random
+from skimage import io
+# import keyboard
 
-reddit = praw.Reddit(client_id='hI4Fwgeo1ghtMA', client_secret='wvNg1MtuMnscZb_7XM_OkiEPmro', user_agent='memeScraper')
-the_memes = []
+r = praw.Reddit(client_id=config.client_id,
+    client_secret=config.client_secret,
+    user_agent=config.user_agent,
+    username=config.username,
+    password=config.password)
 
 subreddits = [
     "dankmemes",
@@ -16,20 +20,32 @@ subreddits = [
     "AdviceAnimals",
     "memes"
 ]
-url = "http://new.reddit.com/r/"
-headers = {'Accept-Encoding': 'identity'}
-# content = [requests.get(url + r, headers=headers) for r in subreddits]
-# soup = [BeautifulSoup(h.text, features="html5lib") for h in content]
-# print(soup)
 
-def getMemes(subreddit):
-    html = requests.get(url+subreddit, headers=headers)
-    soup = BeautifulSoup(html.content, features="html5lib")
-    memes = []
-    for img in soup.findAll('img'):
-        memes.extend(img['src'])
-        print(img['src'])
-    # print(memes)
-    if len(memes) == 0:
-        print(soup)
-getMemes(subreddits[1])
+def getMemes(subreddits):
+    links = []
+    for subreddit in subreddits:
+        for submission in r.subreddit(subreddit).hot(limit=10):
+            links.append(str(submission.url))
+    memes = filter(lambda k: 'i.redd.it' in k, links)
+    random.shuffle(memes)
+    return memes
+
+def displayMeme(url):
+    io.imshow(io.imread(url))
+    io.show()
+
+memes = getMemes(subreddits)
+print(memes)
+# while True:
+#     try: #used try so that if user pressed other than the given key error will not be shown
+#         if keyboard.is_pressed(' '):#if key 'q' is pressed
+#             num = random.randint(0,len(memes))
+#             displayMeme(memes[num])
+#             break#finishing the loop
+#         else:
+#             pass
+#     except:
+#         break #if user pressed a key other than the given key the loop will break
+num = input("How many memes? ")
+for n in range(num):
+    displayMeme(memes[n])
